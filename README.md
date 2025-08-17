106e29630570cfc8eb62ece90f75ac5613b8a503
Fauzi Ramdani <fauziramdhan38@gmail.com>
2025-08-17 19:05:35 +0700

    feature: new company profile 42 Coffee

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# This Branch used for company profile 42 Coffee
+
 # CakePHP Application Skeleton
 
 [![Build Status](https://img.shields.io/travis/cakephp/app/master.svg?style=flat-square)](https://travis-ci.org/cakephp/app)
--- config/app.default.php
+++ config/app.default.php
@@ -257,7 +257,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => '',
-            'database' => 'genx',
+            'database' => '42coffee',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
